a3e5b5402140ebe7bf8a5676751e21e0270e3416
janet <ja.huaylla@duocuc.cl>
2026-06-12 15:44:29 -0400

    update msusuariosaplicationtests

--- ms-usuarios/src/test/java/com/cordillera/msusuarios/MsUsuariosApplicationTests.java
+++ ms-usuarios/src/test/java/com/cordillera/msusuarios/MsUsuariosApplicationTests.java
@@ -1,10 +1,27 @@
 package com.cordillera.msusuarios;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 
 class MsUsuariosApplicationTests {
 
     @Test
-    void main() {
-        MsUsuariosApplication.main(new String[]{});
+    @DisplayName("El metodo main debe ejecutarse sin errores")
+    void main_DeberiaEjecutarSinErrores() {
+        try (MockedStatic<SpringApplication> mocked = 
+                Mockito.mockStatic(SpringApplication.class)) {
+            
+            mocked.when(() -> SpringApplication.run(
+                    MsUsuariosApplication.class, new String[]{}))
+                    .thenReturn(null);
+            
+            MsUsuariosApplication.main(new String[]{});
+            
+            mocked.verify(() -> SpringApplication.run(
+                    MsUsuariosApplication.class, new String[]{}));
+        }
     }
 }
